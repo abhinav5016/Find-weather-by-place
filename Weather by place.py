54b083e7e4a00f6d2593bc5d4399be41f1e0e5a0
Abhinav <abhinav@gmail.com>
2025-11-16 19:49:26 +0530

    update

--- Weather by place.py
+++ Weather by place.py
@@ -1,30 +1,41 @@
 import requests
 
 
-#Convert place name to latitude & longitude---
+# Convert place name to latitude & longitude---
 def get_location(place_name):
-    location_url = f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
+    location_url = (
+        f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
+    )
     headers = {"User-Agent": "PIX AI"}
     response = requests.get(location_url, headers=headers)
     data = response.json()
-    return data[0]['lat'], data[0]['lon']
+    return data[0]["lat"], data[0]["lon"]
 
 
-
-#Get weather information using latitude & longitude
+# Get weather information using latitude & longitude
 def get_weather(latitude, longitude):
     weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m"
     response = requests.get(weather_url)
     data = response.json()
-    return data['current']['temperature_2m']
+    return data["current"]["temperature_2m"]
 
 
-#User input 
-place = input("Enter a place you want the weather for: ")
+# User input & loop function
+while True:
+    print("Hello there...Nice to meet you")
+    place = input("Enter a place you want the weather for: ")
 
-lat, lon = get_location(place)
-temperature = get_weather(lat, lon)
+    lat, lon = get_location(place)
+    temperature = get_weather(lat, lon)
 
-print(f"The current temperature in {place} is {temperature}°C.")
-print("If you want to check another place!")
+    print(f"The current temperature in {place} is {temperature}°C.")
 
+    # Asking user if they want to continue
+    again = input("Do you want to check another place? (yes/no): ")
+
+    if again.lower() != "yes":
+        print("Okay! Thankyou for using PIX weather ai.")
+        break
+
+
+print()
