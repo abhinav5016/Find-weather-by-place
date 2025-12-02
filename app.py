2decb132e6237688fd53a9b5c8904a8a5583c765
Abhinav5016 <abhinav25106@gmail.com>
2025-12-02 15:21:27 +0530

    fix: improve error handling and logging in location and weather API calls

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 from flask import Flask, request, render_template
 import requests
+import logging
 
 app = Flask(__name__)
 
+# Enable logging (shows in Render logs)
+logging.basicConfig(level=logging.INFO)
+
 # Convert place name to latitude & longitude
 def get_location(place_name):
-    url = f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
-    headers = {"User-Agent": "Weather-Web-App"}
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    if not data:
+    try:
+        url = f"https://nominatim.openstreetmap.org/search?format=json&q={place_name}"
+        headers = {"User-Agent": "Mozilla/5.0"}
+        response = requests.get(url, headers=headers)
+
+        logging.info("Location API response: %s",response.text)
+        
+        data = response.json()
+        
+        if not data:
+            return None, None
+        
+        return data[0]["lat"], data[0]["lon"]
+    except Exception as e:
+        logging.error("Location error: %s", e)
         return None, None
-    return data[0]["lat"], data[0]["lon"]
 
 # Get weather by lat/lon
 def get_weather(lat, lon):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m"
-    response = requests.get(url)
-    data = response.json()
-    if "current" not in data:
-        return None
-    return data["current"]["temperature_2m"]
+    try:
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+        response = requests.get(url)
 
-# Web UI
+        logging.info("Weather API response: %s", response.text)
+
+        data = response.json()
+
+        if "current_weather" not in data:
+            return None
+
+        return data["current_weather"]["temperature"]
+
+    except Exception as e:
+        logging.error("Weather error: %s", e)
+        return None
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
